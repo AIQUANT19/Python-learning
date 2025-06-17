b92df70d1bffe383d0c1edcba0a817775cf91bec
Atosi Das <atosi.das@aiquantgroup.com>
2025-06-17 14:44:36 +0530

    Add with more details

--- Pandas/new_env/example1.py
+++ Pandas/new_env/example1.py
@@ -27,11 +27,13 @@ result = pd.Series(prices, index=fruits)
 print(f"The prices of the fruits are \n{result}")
 
 print(result[["Apple", "Orange"]])
-
+print(f"The price of the third fruit is {result["Banana"]}")
+print(f"The sum of the 5 fruits is {result.sum(axis=0)}")
+print(f"the average price is {result.sum(axis=0)/result.count()}")
 print(result[:3])
 print(result[-2:])
 print("Pineapple" in result)
-
+print(f"The fruits taht cost more than 50 is \n{result[result > 50]}")
 result["Banana"] += 10
 print(result)
 
--- Pandas/new_env/example2.py
+++ Pandas/new_env/example2.py
@@ -13,20 +13,39 @@ df = pd.DataFrame(data)
 print(df)
 
 
-print(df.head())  # Get the 5 rows
-
+print(df.head())  # Get first 5 rows
+print(df.tail())  # get last 5 rows
 print(df.columns)   # Get the column names
-
+print(df[:2])
 print(df.shape)  # return the dimensions(rows, columns)
-
-print(df.describe())   # Get statistics of numeric columns
-
+print(f"The average marks in Maths is {df["Math"].sum(axis=0)/df.count()}")
+print(df.describe())   # Get statistics of numeric columns, count, mean, std, min, 25%, 50%, 75%, max
+print(df.info())  # Info about the data types
 print(df["Math"])
-
+print(f"The students whose marks in Maths are greater than 90 is {df[df["Math"]>90]}")
 print(df.Name)
 
-print(df.iloc[1])   # Second row
+print(df.iloc[1])   # Second row  # Row with index 1
 
-print(df.loc[1])
+print(df.loc[1])   # Row at position 1
 
 print(df[1:3]) # second row & third row
+# new_df = df.drop(axis="Math")
+
+print(df)
+
+
+def get_grade(marks):
+    if marks >= 90:
+        return 'A'
+    elif marks >= 80:
+        return 'B'
+    else:
+        return 'C'
+    
+df["Grade_Math"] = df["Math"].apply(get_grade)
+
+print(df)
+
+df = df.drop("Geography", axis= 1)
+print(df)
